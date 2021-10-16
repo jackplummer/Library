d86dcee9c8dc7dc3dbae9221857094ecffb32a8e
Jack Plummer <jackplummer78@gmail.com>
2021-10-16 22:22:35 +0100

    Added ability to add books to library

---
+++ javascript.js
@@ -0,0 +1,99 @@
+// Books
+
+let myLibrary = [];
+const library = document.querySelector('.library');
+let deleteBookButtons = [];
+
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+};
+
+function addBookToLibrary(book) {
+  bookString = `${book.title}, by ${book.author}, ${book.pages} pages long. ${book.read}.`
+  return myLibrary.push(bookString);
+};
+
+function displayLibrary() {
+  library.textContent = '';
+  for (const books in myLibrary) {
+    let book = document.createElement('div');
+    book.classList.add('book-card');
+    book.setAttribute('data-index', `${books}`);
+    book.textContent = myLibrary[books];
+    library.appendChild(book);
+
+    let deleteBook = document.createElement('span'); // delete book
+    deleteBook.innerHTML = '&times;';
+    deleteBook.classList.add('delete-book');
+    deleteBook.setAttribute('data-index', `${books}`);
+    book.appendChild(deleteBook);
+  };
+
+  deleteBookButtons = document.querySelectorAll('.delete-book');
+  deleteBook();
+};
+
+function deleteBookLibrary() {
+  // add function to delete the book from the library
+}
+
+function deleteBook() {
+  for (const deleteBookButton of deleteBookButtons) {
+    deleteBookButton.addEventListener('click', function() {
+      let deleteBookIndex = document.querySelectorAll(`[data-index]`);
+      console.log(deleteBookIndex);
+      for (const deleteBookItem of deleteBookIndex) {
+        if (deleteBookItem.dataset.index === deleteBookButton.dataset.index) {
+          deleteBookItem.remove();
+        };
+      };
+    })
+  };
+};
+
+// Modal
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+const addBook = document.querySelector('#add');
+
+addBook.addEventListener('click', function() {
+  modal.style.display = 'block';
+});
+
+close.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function(event) { // close modal if area around modal is clicked 
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  };
+});
+
+// Modal form
+const submitForm = document.querySelector("#submit");
+const newTitle = document.querySelector("#title");
+const newAuthor = document.querySelector("#author");
+const newPages = document.querySelector("#pages");
+let newRead = document.querySelector('input[name="radio"]:checked');
+
+function clearForm() {
+  newTitle.value = '';
+  newAuthor.value = '';
+  newPages.value = '';
+};
+
+submitForm.addEventListener('click', function() {
+  newRead = document.querySelector('input[name="radio"]:checked');
+  const newBook = new Book(newTitle.value, newAuthor.value, newPages.value, newRead.value);
+  addBookToLibrary(newBook);
+  displayLibrary();
+  clearForm();
+  modal.style.display = 'none';
+});
+
+
+
